22138e8bb153bc185b8a9cbda3e9fa2d0a66b429
takara2314 <takara2314_TK@outlook.jp>
2021-02-01 22:45:53 +0900

    🌁 Add another mesh

--- assets/ts/components/World.tsx
+++ assets/ts/components/World.tsx
@@ -3,18 +3,33 @@ import * as THREE from 'three';
 import { useFrame } from 'react-three-fiber';
 
 function World() {
-  const ref = useRef({} as THREE.Mesh);
-  useFrame(() => (ref.current.rotation.z += 0.01));
+  const ref1 = useRef({} as THREE.Mesh);
+  const ref2 = useRef({} as THREE.Mesh);
+
+  useFrame(() => {
+    ref1.current.rotation.z += 0.01;
+    ref2.current.rotation.z -= 0.01;
+  });
+
   return (
     <>
       <mesh
-        ref={ref}
+        ref={ref1}
         onClick={e => console.log('click')}
         onPointerOver={e => console.log('hover')}
         onPointerOut={e => console.log('unhover')}
       >
         <boxBufferGeometry attach="geometry" args={[0.5, 20, 1]} />
         <meshBasicMaterial attach="material" color="rgb(0, 255, 0)" />
+      </mesh>
+      <mesh
+        ref={ref2}
+        onClick={e => console.log('click')}
+        onPointerOver={e => console.log('hover')}
+        onPointerOut={e => console.log('unhover')}
+      >
+        <boxBufferGeometry attach="geometry" args={[0.5, 20, 1]} />
+        <meshBasicMaterial attach="material" color="rgb(25, 230, 0)" />
       </mesh>
     </>
   );
